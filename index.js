dd93187e9191277edc340a2710afb7b8dc47d07a
verbitskayaZoya <103507783+verbitskayaZoya@users.noreply.github.com>
2024-03-13 22:32:29 +0700

    Refactoring
    Refactored the whole website

--- index.js
+++ index.js
@@ -2,11 +2,51 @@ let timerFirst = document.getElementById('timer-first')
 const btnFirst = document.getElementById('button-first')
 let timerSecond = document.getElementById('timer-second')
 const btnSecond = document.getElementById('button-second')
-console.log(timerFirst, timerSecond)
+
+const hideBtnTimer = document.getElementById('hide-button-timer')
+const hideBtnTextarea = document.getElementById('hide-button-textarea')
+const textareaEl = document.getElementById('writing-section')
+let isHideBtnTimer = true
+let isHideBtnTextarea = true
+
+// HIDE ELEMENTS 
+hideBtnTimer.addEventListener('click', () => {
+  if(isHideBtnTimer) {
+    hideBtnTimer.innerText = `Show 
+                              Timer`
+    timerFirst.style.visibility = 'hidden'
+    btnFirst.style.visibility = 'hidden'
+    timerSecond.style.visibility = "hidden"
+    btnSecond.style.visibility = "hidden"
+    isHideBtnTimer = false
+  } else {
+    timerFirst.style.visibility = "visible"
+    btnFirst.style.visibility = "visible"
+    timerSecond.style.visibility = "visible"
+    btnSecond.style.visibility = "visible"
+    isHideBtnTimer = true
+    hideBtnTimer.innerText = `Hide 
+                              Timer`
+  }
+})
+
+hideBtnTextarea.addEventListener('click', () => {
+  if(isHideBtnTextarea) {
+    hideBtnTextarea.innerText = `Show
+                                  Input`
+    textareaEl.style.visibility = "hidden"
+    isHideBtnTextarea = false
+  } else {
+    textareaEl.style.visibility = "visible"
+    isHideBtnTextarea = true
+    hideBtnTextarea.innerText = `Hide
+                                  Input`
+  }
+
+})
 
 timerFirst.innerText = "01 : 30"
 timerSecond.innerText = "02 : 00"
-
 
 let seconds = 30;
 let count = 90;
@@ -21,6 +61,7 @@ let stateFirst = true
 let stateSecond = true
 
 
+// FIRST TIMER FOR 1 MINUTE AND 30 MIN
 
 btnFirst.addEventListener("click", () => {
  if(stateFirst == true) {
@@ -34,8 +75,25 @@ btnFirst.addEventListener("click", () => {
  }
 })
 
+function makeIterationFirst () {
+  if(count >= 60) {
+     timerFirst.innerText = seconds >= 10 ? "01 : " + seconds : "01 : 0" + seconds 
+     seconds -= 1;
+     count -= 1;
+    intervalFirst = setTimeout(makeIterationFirst, 1000);  // 1 second waiting
+  } else if (count < 60 && count > 0) {
+   console.log(count)
+    timerFirst.innerText = newSeconds >= 10 ? "00 : " + newSeconds : "00 : 0" + newSeconds
+    newSeconds -= 1;
+    count -=1;
+    intervalFirst = setTimeout(makeIterationFirst, 1000);  // 1 second waiting
+  } else {
+       timerFirst.innerText = "Done!"
+       console.log('Done!');
+   }
+};
 
-
+// SECOND TIMER FOR 2 MINUTES
 btnSecond.addEventListener("click", () => {
   if(stateSecond == true) {
     btnSecond.innerText = "Stop"
@@ -47,26 +105,6 @@ btnSecond.addEventListener("click", () => {
     stateSecond = true
    }
   })
-
-
-function makeIterationFirst () {
-  
-   if(count >= 60) {
-      timerFirst.innerText = seconds >= 10 ? "01 : " + seconds : "01 : 0" + seconds 
-      seconds -= 1;
-      count -= 1;
-     intervalFirst = setTimeout(makeIterationFirst, 1000);  // 1 second waiting
-   } else if (count < 60 && count > 0) {
-    console.log(count)
-     timerFirst.innerText = newSeconds >= 10 ? "00 : " + newSeconds : "00 : 0" + newSeconds
-     newSeconds -= 1;
-     count -=1;
-     intervalFirst = setTimeout(makeIterationFirst, 1000);  // 1 second waiting
-   } else {
-        timerFirst.innerText = "Done!"
-        console.log('Done!');
-    }
-};
 
 function makeIterationSecond () {
   console.log(countSecond)
@@ -89,18 +127,13 @@ function makeIterationSecond () {
        console.log('Done!');
    }
 };
+// TO START A TIMER AUTOMATICALLY: //  setTimeout(makeIterationFirst, 1000);  // 1 second waiting
 
-// setTimeout(makeIteration, 1000);  // 1 second waiting
-
+// TO UPLOAD AND DISPLAY AN IMAGE TO THE PAGE
 const inputElOne = document.getElementById('file-one')
 const fileLabelOne = document.getElementById('file-label-one')
 
-// const inputElTwo = document.getElementById('file-two')
-// const fileLabelTwo = document.getElementById('file-label-two')
-
 inputElOne.addEventListener('change', loadFileOne)
-// inputElTwo.addEventListener('change', loadFileTwo)
-
 
 function loadFileOne(event) {
   fileLabelOne.style.display = "none";
@@ -109,12 +142,7 @@ function loadFileOne(event) {
     imageOne.src = URL.createObjectURL(event.target.files[0]);
 };
 
-// function loadFileTwo(event) {
-//   fileLabelTwo.style.display = "none";
-// 	const imageTwo = document.getElementById('output-two');
-//   imageTwo.style.display = "block";
-//     imageTwo.src = URL.createObjectURL(event.target.files[0]);
-// };
+
 
 
  
